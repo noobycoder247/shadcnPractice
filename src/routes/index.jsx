c33f673203e32a16c369e19cc27e640af43d8eac
sandip mahato <sandip.sizemetric@outlook.com>
2024-06-11 18:55:29 +0530

    added all products page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,6 +5,7 @@ import Layout from "@/components/layout/Layout.jsx";
 import Dashboard from "@/pages/user/Dashboard.jsx";
 import {Login} from "@/pages/user/Login.jsx";
 import {Signup} from "@/pages/user/Signup.jsx";
+import {AllProducts} from "@/pages/product/AllProducts.jsx";
 
 const RoutesDirectory = () => {
     return (
@@ -20,6 +21,7 @@ const RoutesDirectory = () => {
                     <Route path='' element={<Dashboard/>}/>
                     <Route path='login' element={<Login/>}/>
                     <Route path='signup' element={<Signup/>}/>
+                    <Route path='all-products' element={<AllProducts/>}/>
                 </Route>
             </Routes>
 
